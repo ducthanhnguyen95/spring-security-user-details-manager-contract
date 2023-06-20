6ce74f403f2fc77f29fc1610952b688a4b033a9a
thanhnd <ducthanhnguyen95@gmail.com>
2023-06-21 04:23:34 +0700

    Changing JdbcUserDetailsManagers queries to find the user

--- src/main/java/com/example/user/details/manager/contract/config/ProjectConfig.java
+++ src/main/java/com/example/user/details/manager/contract/config/ProjectConfig.java
@@ -12,12 +12,25 @@ import javax.sql.DataSource;
 @Configuration
 public class ProjectConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        String usersByUsernameQuery =
+                "select username, password, enabled from users where username = ?";
+        String authsByUserQuery =
+                "select username, authority from spring.authorities where username = ?";
+
+        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+        return userDetailsManager;
     }
 }
