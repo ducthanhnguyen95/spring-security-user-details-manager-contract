de416ba78aaf66744165f35919628139f2b1f2a2
thanhnd <ducthanhnguyen95@gmail.com>
2023-06-21 05:14:37 +0700

    Using an Ldapuserdetailsmanager for user management

--- src/main/java/com/example/user/details/manager/contract/config/ProjectConfig.java
+++ src/main/java/com/example/user/details/manager/contract/config/ProjectConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.ldap.DefaultLdapUsernameToDnMapper;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.ldap.userdetails.LdapUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
@@ -13,8 +16,16 @@ import javax.sql.DataSource;
 public class ProjectConfig {
 
     @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
+    public UserDetailsService userDetailsService() {
+        var cs = new DefaultSpringSecurityContextSource(
+                "ldap://127.0.0.1:33389/dc=springframework,dc=org");
+        cs.afterPropertiesSet();
+        var manager = new LdapUserDetailsManager(cs);
+        manager.setUsernameMapper(
+                new DefaultLdapUsernameToDnMapper("ou=groups", "uid"));
+        manager.setGroupSearchBase("ou=groups");
+
+        return manager;
     }
     @Bean
     public PasswordEncoder passwordEncoder() {
