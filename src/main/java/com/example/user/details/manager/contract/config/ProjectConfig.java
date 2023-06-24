ef464664f089c0afca8d3db5d8f7cbe6fd2bcf60
thanhnd <ducthanhnguyen95@gmail.com>
2023-06-24 10:16:14 +0700

    Optimize library

--- src/main/java/com/example/user/details/manager/contract/config/ProjectConfig.java
+++ src/main/java/com/example/user/details/manager/contract/config/ProjectConfig.java
@@ -3,17 +3,11 @@ package com.example.user.details.manager.contract.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class ProjectConfig {
